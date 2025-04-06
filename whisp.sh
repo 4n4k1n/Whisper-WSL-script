8af443dc6ecf26e7ee50833d9b747284f7dbf1b3
4n4k1n <anakin@pregitzer.de>
2025-04-06 12:13:56 +0000

    changed the path to one up

--- whisp.sh
+++ whisp.sh
@@ -2,7 +2,7 @@
 
 source ~/whisper-env/bin/activate
 
-echo "Enter your source path (<username>\...):"
+echo "Enter your source path (USERS/<username>/...):"
 read src_dir
 
 echo "Enter the language (english/german/...):"
@@ -14,10 +14,10 @@ read model
 echo "Enter the output format (txt/...):"
 read dst_format
 
-echo "Enter the output directory (<username>\...):"
+echo "Enter the output directory (USERS/<username>/...):"
 read dst_dir
 
 
-whisper "/mnt/c/Users/$src_dir" --language "$language" --model "$model" --output_format "$dst_format" --output_dir "/mnt/c/Users/$dst_dir"
+whisper "/mnt/c/$src_dir" --language "$language" --model "$model" --output_format "$dst_format" --output_dir "/mnt/c/Users/$dst_dir"
 
 deactivate
