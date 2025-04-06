4d66128759fdc67dcb1c5e4569aa0ef60cbd9881
4n4k1n <anakin@pregitzer.de>
2025-04-06 11:11:30 +0000

    script to setup the wisp file

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+chmod 777 .bashrc
+touch ~/whisp.sh
+curl -L https://raw.githubusercontent.com/4n4k1n/Whisper-WSL-script/refs/heads/main/whisp.sh > ~/whisp.sh
+chmod 777 ~/whisp.sh
+echo "alias wisp='bash ~/whisp.sh'" >> ~/.bashrc
