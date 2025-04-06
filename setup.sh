3001838c53ae0a34cdfe43e7023ea4741ff2b03c
4n4k1n <anakin@pregitzer.de>
2025-04-06 11:17:26 +0000

    fixed the url

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 chmod 777 .bashrc
 touch ~/whisp.sh
-curl -L https://raw.githubusercontent.com/4n4k1n/Whisper-WSL-script/refs/heads/main/whisp.sh > ~/whisp.sh
+curl -L https://raw.githubusercontent.com/4n4k1n/Whisper-WSL-script/main/whisp.sh > ~/whisp.sh
 chmod 777 ~/whisp.sh
 echo "alias wisp='bash ~/whisp.sh'" >> ~/.bashrc
